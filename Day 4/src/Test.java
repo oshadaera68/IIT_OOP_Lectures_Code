8ceb4073b17460bbf9d5bc6ed70bada536a52085
oshadaera68 <mindartlk010@gmail.com>
2024-10-16 12:45:01 +0530

    updated code

---
+++ Day 4/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Coded By: Era Boy
+ * Version: v0.1.0
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        Student obj = new Student("Era", "Boy", "12345");
+    }
+}
