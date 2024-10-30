b964f63a275c6ce6c8e4be2b670329d88a8440a8
oshadaera68 <mindartlk010@gmail.com>
2024-10-30 13:10:22 +0530

    6

---
+++ Day 6/src/Animal.java
@@ -0,0 +1,10 @@
+/**
+ * Coded By: Era Boy
+ * Version: v0.1.0
+ **/
+
+public class Animal {
+    public void eat(){
+
+    }
+}
---
+++ Day 6/src/Mammal.java
@@ -0,0 +1,8 @@
+/**
+ * Coded By: Era Boy
+ * Version: v0.1.0
+ **/
+
+public abstract class Mammal extends Animal{
+    public abstract void feedMilk();
+}
