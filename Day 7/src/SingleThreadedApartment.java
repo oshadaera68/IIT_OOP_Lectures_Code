4ee8ac32736c29a9ed507a933cc3c87b5d8031cf
oshadaera68 <mindartlk010@gmail.com>
2024-11-06 12:20:12 +0530

    updated

---
+++ Day 7/src/SingleThreadedApartment.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Coded By: Era Boy
+ * Version: v0.1.0
+ **/
+
+public class SingleThreadedApartment {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+
+//        for (int i = 2; i > 1 ; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (;;) {
+//            System.out.println("Hello World");
+//        } Infinite loop
+    }
+}
